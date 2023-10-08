1514a5ee6eea00950f9f76525d743ec1dbf9a849
Mike <spormuv@gmail.com>
2023-10-09 00:59:36 +0300

    feat: add simple api

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 //////////////////////////////////////////// FILES
 
@@ -32,9 +33,28 @@ const http = require('http');
 // console.log('Will read file!');
 
 //////////////////////////////////////////// SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-type': 'text/html' });
-  res.end('<div>Hello from the server!</div>');
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW page');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT page');
+  } else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(3000, '127.0.0.1', () => {
