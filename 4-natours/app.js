ae1870b6bfc36924c16c2bc885561796f3f7feaf
Mike <spormuv@gmail.com>
2023-11-20 00:33:13 +0300

    feat: handling get requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,16 +1,31 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Hello World!',
-    app: 'Natours',
-  });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Hello World!',
+//     app: 'Natours',
+//   });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(PORT, () => {
