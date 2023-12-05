114b33a930f78e77a838f87ffb275510cb28b07a
Mike <spormuv@gmail.com>
2023-12-06 01:26:53 +0300

    feat: add post route

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({
 //     message: 'Hello World!',
@@ -26,6 +28,29 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = {
+    id: newId,
+    ...req.body,
+  };
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(PORT, () => {
