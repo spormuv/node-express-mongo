3c80e22730c34989120bce02bf8b0617cec393d2
Mike <spormuv@gmail.com>
2023-12-30 20:46:35 +0300

    feat: connect the mongoose

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,5 +1,24 @@
-require('dotenv').config({ path: './config.env' });
+/* eslint-disable import/no-extraneous-dependencies */
+// require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 // console.log(app.get('env'));
 const PORT = process.env.PORT || 3000;
