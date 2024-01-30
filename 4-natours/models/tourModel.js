ef181a13009c8d3175df5aad137c2cf8f52c39b5
Mike <spormuv@gmail.com>
2024-01-30 23:05:56 +0300

    feat: add mongoose document middleware

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,4 +1,7 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -60,9 +64,26 @@ const tourSchema = new mongoose.Schema(
   },
 );
 
+// VIRTUAL PROPERTY
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
