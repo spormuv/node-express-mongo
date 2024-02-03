ae9a3a9408ed74ff0ee877f2143ca5ddee82fcb7
Mike <spormuv@gmail.com>
2024-02-04 01:42:03 +0300

    fix: remove unnecessary console.log

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -95,9 +95,8 @@ tourSchema.post(/^find/, function (docs, next) {
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function (next) {
-  console.log(this);
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  console.log(this.pipeline());
+  // console.log(this.pipeline());
   next();
 });
 
