255fc115cb4fd24a57874df89513782141e25ec7
Mike <spormuv@gmail.com>
2024-02-28 02:04:23 +0300

    feat: add catching errors in async func

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,7 +1,7 @@
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
 
-  err.stausCode = err.statusCode || 500;
+  err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   res.status(err.statusCode).json({
