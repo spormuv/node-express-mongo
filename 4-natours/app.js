4ea7fe68739a62cc360e98be5f0ac574f21994e3
Mike <spormuv@gmail.com>
2024-02-29 21:15:51 +0300

    feat: handling invalid database IDs

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -9,7 +9,7 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
-process.env.NODE_ENV === 'development' && app.use(morgan('dev'));
+process.env.NODE_ENV.trim() === 'development' && app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
