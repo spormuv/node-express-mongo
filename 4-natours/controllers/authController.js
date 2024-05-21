c9acc8a905e021327c07f8711d0b403a91816083
Mike <spormuv@gmail.com>
2024-05-22 01:19:18 +0300

    feat: add password reset functionality

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -105,3 +105,17 @@ exports.restrictTo =
     }
     next();
   };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with email address.', 404));
+  }
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  // 3) Send it to the user's email
+});
+exports.resetPassword = (req, res, next) => {};
