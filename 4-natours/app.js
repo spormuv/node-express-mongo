04365ededfc0df53e4683b012daf31bf1750ee50
Mike <spormuv@gmail.com>
2024-06-11 20:36:27 +0300

    feat: Implement Rate Limiting

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,8 +9,16 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middlewares
+// Global Middlewares
 process.env.NODE_ENV.trim() === 'development' && app.use(morgan('dev'));
+
+const limiter = rateLimit({
+  limit: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
