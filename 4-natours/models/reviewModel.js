efa1bcf8e1f1f994412ec0a1196e1172c266ed9c
Mike <spormuv@gmail.com>
2024-07-09 00:44:32 +0300

    feat: Prevent Duplicate Reviews

--- 4-natours/models/reviewModel.js
+++ 4-natours/models/reviewModel.js
@@ -33,6 +33,16 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.index(
+  {
+    tour: 1,
+    user: 1,
+  },
+  {
+    unique: true,
+  },
+);
+
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
   //   path: 'tour',
