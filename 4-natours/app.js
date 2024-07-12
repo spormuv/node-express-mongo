538c25077aa8f744921d6d40ebfa15c623bf6762
Mike <spormuv@gmail.com>
2024-07-12 22:23:35 +0300

    feat: Setting up the Project Structure

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const vewRouter = require('./routes/vewRoutes');
 
 const app = express();
 
@@ -72,13 +73,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Mike',
-  });
-});
-
+app.use('/', vewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
