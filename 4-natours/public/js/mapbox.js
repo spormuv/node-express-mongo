557c848cb248e887c120536011b7bd9cbbd8a036
Mike <spormuv@gmail.com>
2024-07-21 23:18:07 +0300

    feat: Include a Map with Mapbox

--- 4-natours/public/js/mapbox.js
+++ 4-natours/public/js/mapbox.js
@@ -1,4 +1,73 @@
-/* eslint-disable no-undef */
+/* eslint-disable */
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+// mapboxgl.accessToken =
+//   'pk.eyJ1IjoiZGVsdGEtc3R1ZHVlbnQiLCJhIjoiY2xvMDk0MTVhMTJ3ZDJrcGR5ZDFkaHl4ciJ9.Gj2VU1wvxc7rFVt5E4KLOQ';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  // style: 'mapbox://styles/mapbox/standard',
+  style: {
+    version: 8,
+    sources: {
+      'raster-tiles': {
+        type: 'raster',
+        tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
+        tileSize: 256,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      },
+    },
+    layers: [
+      {
+        id: 'simple-tiles',
+        type: 'raster',
+        source: 'raster-tiles',
+        minzoom: 0,
+        maxzoom: 22,
+      },
+    ],
+  },
+  zoom: 1,
+  scrollZoom: false,
+  // center: [-118.132843, 34.144105],
+  // interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
