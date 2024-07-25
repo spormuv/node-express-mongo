04db25705a8afab0ca5e8b2e41fb0f28a3e24fb7
Mike <spormuv@gmail.com>
2024-07-26 00:04:28 +0300

    feat: implement res.locals and user image in header

--- 4-natours/public/js/login.js
+++ 4-natours/public/js/login.js
@@ -1,7 +1,5 @@
 /* eslint-disable */
 const login = async (email, password) => {
-  console.log(email, password);
-
   try {
     const res = await axios({
       method: 'post',
@@ -11,9 +9,15 @@ const login = async (email, password) => {
         password,
       },
     });
-    console.log(res);
+    // console.log(res);
+    if (res.data.status === 'success') {
+      alert('Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
   } catch (err) {
-    console.log(err.response.data);
+    alert(err.response.data.message);
   }
 };
 
