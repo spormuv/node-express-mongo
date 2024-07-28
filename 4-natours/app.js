b064fb4ade7bf62977e57194830d10efea9dd644
Mike <spormuv@gmail.com>
2024-07-28 20:22:53 +0300

    feat: Update User Data with API

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -80,6 +80,7 @@ app.use(
     limit: '10kb',
   }),
 );
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const User = require('../models/userModel');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
@@ -45,3 +46,22 @@ exports.getAccount = (req, res) => {
     title: 'Your account',
   });
 };
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser,
+  });
+});
